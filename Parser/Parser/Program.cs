33dbecf19bbf25e6d2d6b166c86f13aa7834b14c
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-05-07 11:01:02 +0700

    update code

--- Parser/Parser/Program.cs
+++ Parser/Parser/Program.cs
@@ -17,7 +17,8 @@ namespace Parser
     {
         private static void Main(string[] args)
         {
-            LeadsFolder();
+            ORDERS_CC_AUTHORIZE();
+            //LeadsFolder();
         }
 
         public static void TrimHtmlTag()
@@ -96,14 +97,17 @@ namespace Parser
             var i = 0;
             try
             {
-                foreach (string fileName in Directory.GetFiles("D:\\SourceCode\\Simple-Text-Parser\\Parser\\Data\\test"))
+                foreach (string fileName in Directory.GetFiles("D:\\SourceCode\\Simple-Text-Parser\\Parser\\Data\\LEADS")
+                )
                 {
                     string[] fileLines = File.ReadAllLines(fileName);
                     StreamReader sr = new StreamReader(fileName);
 
                     foreach (var line in fileLines)
                     {
-                        MatchCollection matchCollections = Regex.Matches(line, "(id|email|status|Email|IP Address|IP Address|city|country|first_name|has_read_LBoT|has_read_TC|has_read_TF|last_name|last_trading_or_investment_book_read|state|street1|street2|zip_code|Date|utc_offset|visitor_uuid|time_zone|friendly_time_zone|tags|created_at|lead_score|time_zone)(:)( )?([ A-Za-z0-9\\/\\&\\+\\,\\:\\@\\.\\\"\\-\\&\\[\\]]+)?", RegexOptions.Multiline);
+                        MatchCollection matchCollections = Regex.Matches(line,
+                            "(id|email|status|Email|IP Address|IP Address|city|country|first_name|has_read_LBoT|has_read_TC|has_read_TF|last_name|last_trading_or_investment_book_read|state|street1|street2|zip_code|Date|utc_offset|visitor_uuid|time_zone|friendly_time_zone|tags|created_at|lead_score|time_zone)(:)( )?([ A-Za-z0-9\\/\\&\\+\\,\\:\\@\\.\\\"\\-\\&\\[\\]]+)?",
+                            RegexOptions.Multiline);
 
                         foreach (var lineResult in matchCollections)
                         {
@@ -133,7 +137,8 @@ namespace Parser
                                 string elementName = result[0].Replace(" ", "-");
                                 string elementValue = result[1];
                                 elementValue = elementValue == string.Empty ? "n/a" : result[1];
-                                XmlElement element = xmlDocument.CreateElement(string.Empty, elementName.ToLower(), string.Empty);
+                                XmlElement element = xmlDocument.CreateElement(string.Empty, elementName.ToLower(),
+                                    string.Empty);
                                 element.InnerText = elementValue;
                                 itemElement.AppendChild(element);
                             }
@@ -142,8 +147,96 @@ namespace Parser
                         }
                     }
 
-                    var fileNameExtract = Path.GetFileNameWithoutExtension(fileName);
-                    xmlDocument.Save($"D:\\SourceCode\\Simple-Text-Parser\\Parser\\Data\\Result\\{fileNameExtract}.xml");
+                    xmlDocument.Save($"D:\\SourceCode\\Simple-Text-Parser\\Parser\\Data\\Result\\LEADS.xml");
+                    i = 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static void ORDERS_CC_AUTHORIZE()
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            XmlDeclaration xmlDeclaration = xmlDocument.CreateXmlDeclaration("1.0", "UTF-8", null);
+            XmlElement root = xmlDocument.DocumentElement;
+            xmlDocument.InsertBefore(xmlDeclaration, root);
+            XmlElement bodyElement = xmlDocument.CreateElement(string.Empty, "items", string.Empty);
+            xmlDocument.AppendChild(bodyElement);
+            XmlElement itemElement = null;
+            var i = 0;
+            try
+            {
+                foreach (string fileName in Directory.GetFiles("D:\\SourceCode\\Simple-Text-Parser\\Parser\\Data\\DRIP_PHUC\\ORDERS_CC_AUTHORIZE.NET")
+                )
+                {
+                    string[] fileLines = File.ReadAllLines(fileName);
+                    StreamReader sr = new StreamReader(fileName);
+
+                    foreach (var line in fileLines)
+                    {
+                        MatchCollection matchCollections = Regex.Matches(line,
+                            "(Date|Merchant|Date/Time|Invoice|Description|Amount|Payment Method|Transaction Type|Response|Auth Code|Transaction ID|Address Verification|Customer ID|First Name|Last Name|Company|Address|City|State/Province|Zip/Postal Code|Country|Phone|Fax|E-Mail|First Name|Last Name|Company|Address|City|State/Province|Postal Code|Country|Tax|Duty|Freight|Tax Exempt|PO Number)( )?(:)?( )?([ A-Za-z0-9\\/\\&\\+\\,\\:\\@\\.\\\"\\-\\&\\[\\](\\\\)\\-]+)?",
+                            RegexOptions.Multiline);
+                        foreach (var lineResult in matchCollections)
+                        {
+                            var lineResultParse = lineResult.ToString();
+
+                            if (lineResultParse.Equals(string.Empty)) continue;
+
+                            var result = lineResultParse.Split(new[] { ":" }, 2, StringSplitOptions.None);
+                            if (result.Length == 1)
+                            {
+                                if (result[0] == string.Empty)
+                                {
+                                    i = 0;
+                                    continue;
+                                }
+                                continue;
+                            }
+
+                            if (i == 0)
+                            {
+                                itemElement = xmlDocument.CreateElement(string.Empty, "item", string.Empty);
+                                bodyElement.AppendChild(itemElement);
+                            }
+
+                            if (itemElement != null)
+                            {
+                                string elementName = string.Empty;
+                                //\s matches whitespace
+                                //+ means one or more of the preceding expression
+                                //(one or more whitespace characters)
+                                //$ means the end of the string
+                                if (Regex.IsMatch(result[0], @"\s+$"))
+                                {
+                                    elementName = result[0].Replace(" ", "");
+                                    elementName = elementName.Replace(elementName.Contains("/") ? "/" : " ", "-");
+                                    string elementValue = result[1];
+                                    elementValue = elementValue == " " ? "n/a" : result[1];
+                                    XmlElement element = xmlDocument.CreateElement(string.Empty, elementName.ToLower(),
+                                        string.Empty);
+                                    element.InnerText = elementValue;
+                                    itemElement.AppendChild(element);
+                                }
+                                else
+                                {
+                                    elementName = result[0].Replace(" ", "-");
+                                    string elementValue = result[1];
+                                    elementValue = elementValue == string.Empty ? "n/a" : result[1];
+                                    XmlElement element = xmlDocument.CreateElement(string.Empty, elementName.ToLower(),
+                                        string.Empty);
+                                    element.InnerText = elementValue;
+                                    itemElement.AppendChild(element);
+                                }
+                            }
+
+                            i++;
+                        }
+                    }
+                    xmlDocument.Save($"D:\\SourceCode\\Simple-Text-Parser\\Parser\\Data\\Result\\ORDERS_CC_AUTHORIZE.xml");
                     i = 0;
                 }
             }
