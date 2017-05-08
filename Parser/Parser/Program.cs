e5e7744c27a32afcec12418a3ef40bfe3559a5c0
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-05-08 21:44:44 +0700

    Get email list

--- Parser/Parser/Program.cs
+++ Parser/Parser/Program.cs
@@ -17,8 +17,7 @@ namespace Parser
     {
         private static void Main(string[] args)
         {
-            ORDERS_CC_AUTHORIZE();
-            //LeadsFolder();
+            UnknownEmailList();
         }
 
         public static void TrimHtmlTag()
@@ -244,6 +243,38 @@ namespace Parser
             {
                 throw;
             }
+        }
+
+        public static void UnknownEmailList()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"D:\SourceCode\Simple-Text-Parser\Parser\Data\DRIP_PHUC\LEADS_EUDORA\unknown email list");
+
+            XmlDocument xmlDocument = new XmlDocument();
+            XmlDeclaration xmlDeclaration = xmlDocument.CreateXmlDeclaration("1.0", "UTF-8", null);
+            XmlElement root = xmlDocument.DocumentElement;
+            xmlDocument.InsertBefore(xmlDeclaration, root);
+            XmlElement bodyElement = xmlDocument.CreateElement(string.Empty, "items", string.Empty);
+            xmlDocument.AppendChild(bodyElement);
+
+            var i = 0;
+
+            XmlElement itemElement = null;
+            foreach (string line in lines)
+            {
+                if (i == 0)
+                {
+                    itemElement = xmlDocument.CreateElement(string.Empty, "item", string.Empty);
+                    bodyElement.AppendChild(itemElement);
+                }
+
+                string elementValue = line;
+                XmlElement element = xmlDocument.CreateElement(string.Empty, "email", string.Empty);
+                element.InnerText = elementValue;
+                itemElement.AppendChild(element);
+
+                i++;
+            }
+            xmlDocument.Save(@"D:\SourceCode\Simple-Text-Parser\Parser\Data\Result\UnknownEmailList.xml");
         }
     }
 }
